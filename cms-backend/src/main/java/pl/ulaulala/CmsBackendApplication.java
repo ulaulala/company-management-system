a9c08e3a1385198333ad206ed1f2942fee9e7624
Ula <ukazmierczyk@gmail.com>
2017-03-22 20:52:02 +0100

    Initial commit

---
+++ cms-backend/src/main/java/pl/ulaulala/CmsBackendApplication.java
@@ -0,0 +1,12 @@
+package pl.ulaulala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsBackendApplication.class, args);
+	}
+}
