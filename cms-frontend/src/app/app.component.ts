1f14a7fbcad1a0767be343e3887f6ef2e72cef51
ulaulala <ukazmierczyk@gmail.com>
2017-05-08 11:02:06 +0200

    styles

--- cms-frontend/src/app/app.component.ts
+++ cms-frontend/src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', '../assets/css/bar.css', '../assets/css/reports-style.css']
+  styleUrls: [
+    './app.component.css',
+    '../assets/css/bar.css',
+    '../assets/css/reports-style.css']
 })
 export class AppComponent {
   title = 'app works!';
